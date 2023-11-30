51667d2dcf4851778680b08b7d0dcd52ae66202f
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2023-11-30 16:13:57 +0900

    feat: 리뷰작성 일자 포멧 및 수정

--- src/main/java/com/example/ajouparking/DTO/ReviewDTO.java
+++ src/main/java/com/example/ajouparking/DTO/ReviewDTO.java
@@ -1,5 +1,6 @@
 package com.example.ajouparking.DTO;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -8,6 +9,8 @@ import java.time.LocalDateTime;
 public class ReviewDTO {
     private String author;
     private String reviewText;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDateTime timestamp;
 
     public void setAuthor(String author) {
