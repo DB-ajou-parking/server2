83bbe8c25d616276363d50291e9be02e5c5e8a8f
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2023-12-07 01:33:56 +0900

    ㅁããfeat: 리ë·상세 버튼 누르면 모달 나오고 리뷰 데이터 ã get

--- src/main/java/com/example/ajouparking/controller/FavoriteController.java
+++ src/main/java/com/example/ajouparking/controller/FavoriteController.java
@@ -4,7 +4,6 @@ import com.example.ajouparking.dto.CommonResponseDto;
 import com.example.ajouparking.dto.CustomUserDetails;
 import com.example.ajouparking.entity.User;
 import com.example.ajouparking.service.FavoriteService;
-import com.example.ajouparking.service.LikesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/example/ajouparking/dto/ReviewDto.java
+++ src/main/java/com/example/ajouparking/dto/ReviewDto.java
@@ -1,11 +1,8 @@
 package com.example.ajouparking.dto;
 
 import com.example.ajouparking.entity.Review;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter @Setter
 public class ReviewDto {
